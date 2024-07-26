d2f032ccd0843a563a7393edd05ae682d13b9614
80ps <98580302+80ps@users.noreply.github.com>
2024-07-26 11:22:40 +0900

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,15 @@
 // Create button element
-const btnsChars = ['all', 'bag', 'shoe', 'watch', 'camera', 'headphone'];
+const btnChars = ['all', 'bag', 'shoe', 'watch', 'camera', 'headphone'];
 const btnsWrapper = document.querySelector('.filter-btns');
 
-btnsChars.map(function (btnsChar) {
-  const modifiedChar = btnsChar.charAt(0).toUpperCase() + btnsChar.slice(1);
+btnChars.map(function (btnChar) {
+  const modifiedChar = btnChar.charAt(0).toUpperCase() + btnChar.slice(1);
   // charAt(): 파라미터 인덱스에 해당하는 문자 선택
   // toUpperCase(): 대문자로 변환
   // toLowerCase(): 소문자로 변환
   // slice(): 문자열을 추출 - 파라미터 인덱스부터 추출
 
-  const btnElement = `<button class="filter-btn" data-filter="${btnsChar}">${modifiedChar}</button>`;
+  const btnElement = `<button class="filter-btn" data-filter="${btnChar}">${modifiedChar}</button>`;
   btnsWrapper.insertAdjacentHTML('beforeend', btnElement);
 });
 
@@ -45,7 +45,7 @@ images.map(function (image) {
 
 const imageElements = document.querySelectorAll('.filter-image');
 
-//Filter images
+// Filter images
 function activateFilter() {
   btns.forEach((btn) => {
     btn.classList.remove('active');
@@ -54,26 +54,68 @@ function activateFilter() {
 
   const selectedBtn = this.getAttribute('data-filter');
 
-  //mpa, filter, reduce 함수는 DOM 요소에 해당할 수 없다. 구래서 Array.from()을 사용하여 배열로 변환하다.
-  //images에 대한 filter 적용시켜줄 예정
-  //=== 비교연산자
-
+  // map, filter, reduce 함수는 DOM 요소에 사용할 수 없다. 그래서 Array.from()을 사용하여 배열로 변환한다.
   Array.from(imageElements).filter((imageElement) => {
     imageElement.classList.add('hide');
+    // imageElement.classList.remove('show');
 
-    if (
-      imageElement.getAttribute('data-filter') === selectedBtn ||
-      selectedBtn === 'all'
-    ) {
-      imageElement.classList.remove('hide');
-      imageElement.classList.add('show');
-    } else {
-      imageElement.classList.remove('show');
-      imageElement.classList.add('hide');
-    }
+    setTimeout(() => {
+      if (
+        imageElement.getAttribute('data-filter') === selectedBtn ||
+        selectedBtn === 'all'
+      ) {
+        imageElement.classList.remove('hide');
+        imageElement.classList.add('show');
+      } else {
+        imageElement.classList.remove('show');
+        imageElement.classList.add('hide');
+      }
+    }, 100); // 시간 지연 함수(promise): 첫번째 파라미터 = callback function, 두번째 파라미터 = 시간(밀리초)
   });
 }
+
 btns.forEach(function (btn) {
   btn.addEventListener('click', activateFilter);
 });
-// active를 지우고 내가 필요한 것만 다시 넣어줌
+
+// activate light box when click each image
+const lightBox = document.querySelector('.light-box');
+const overlay = document.querySelector('.overlay');
+
+const showLightBox = (e) => {
+  const target = e.currentTarget;
+  const selectedImage = target.children[0].children[0].getAttribute('src');
+  const categoryName = target.getAttribute('data-filter');
+
+  const lightBoxImage = document.querySelector('.light-box-image img');
+  const categoryElement = document.querySelector('.title p');
+
+  // getAttribute(): 파라미서 속성 값 가져오기
+  // setAttribute(a, b): a: 속성 이름, b: 변경할 속성 값
+  // a.textContent = b: a 요소에 b 텍스트 입력
+
+  lightBoxImage.setAttribute('src', selectedImage);
+  categoryElement.textContent = categoryName;
+
+  lightBox.style.display = 'block';
+  overlay.style.display = 'block'; //1
+};
+
+imageElements.forEach((imageElement) => {
+  imageElement.addEventListener('click', showLightBox);
+});
+
+//close light box
+const closeBtn = document.querySelector('.close');
+
+const closeLightBox = () => {
+  lightBox.style.display = 'none';
+  overlay.style.display = 'none'; //3
+};
+
+// closeBtn.addEventListener('click', closeLightBox);
+// overlay.addEventListener('click', closeLightBox); //2
+
+[closeBtn, overlay].forEach((element) =>
+  element.addEventListener('click', closeLightBox)
+);
